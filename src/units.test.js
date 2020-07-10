f4c311fbd955ab076132727a2a44e9834b3dcc16
pond823 <sasha.bilton@axeinitiativegames.net>
2020-07-10 12:00:25 +0100

    Fixed tests after name changes

--- src/units.test.js
+++ src/units.test.js
@@ -6,14 +6,14 @@ test('get 1st unit, which should be Chaos Marauders', () => {
 
     unit = units.getUnitByIndex(1)
 
-    expect(unit.name).toBe(`Chaos Marauders`)
+    expect(unit.name).toBe(`Chaos Marauders with Barbarian Axes`)
 })
 
-test('get Stabbas by name', () => {
+test('get Chaos Knights by name', () => {
 
-    unit = units.getUnitByName(`Stabbas`)
+    unit = units.getUnitByName(`Chaos Knights with Ensorcelled Weapon`)
 
-    expect(unit.name).toBe(`Stabbas`)
+    expect(unit.name).toBe(`Chaos Knights with Ensorcelled Weapon`)
 })
 
 test('get Saurus Warriors first attack', () => {
